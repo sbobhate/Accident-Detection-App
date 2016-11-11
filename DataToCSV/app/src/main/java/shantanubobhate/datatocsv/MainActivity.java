157cda8419b821db9a2c00b12faff3e8261d01a4
sbobhate <sbobhate@bu.edu>
2016-11-11 00:42:42 -0500

    Detecting in a Service
    Added the sensor data gathering and recording code to a Bound Service.
    This was to address the issue of being able to collect data even when
    the app was not in the foreground.

--- DataToCSV/app/src/main/java/shantanubobhate/datatocsv/MainActivity.java
+++ DataToCSV/app/src/main/java/shantanubobhate/datatocsv/MainActivity.java
@@ -1,14 +1,16 @@
 package shantanubobhate.datatocsv;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.opencsv.CSVWriter;
 
@@ -16,7 +18,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity  {
 
 
     /*
@@ -32,15 +34,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
      *
      */
 
-    private FileWriter mFileWriter;
     private EditText editTextFilename, editTextDescription;
     private Button buttonTracking;
     private Boolean isTracking = false;
-    private File dir;
-    private CSVWriter writer;
-    private String[] dataContainer = {"Acceleration X", "Acceleration Y", "Acceleration Z", "..."};
-    private Sensor accelerometer;
-    private SensorManager mSensorManger;
+
+    private SensorService mLocalService;
+    private boolean isBound = false;
+
+    private ServiceConnection myConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            SensorService.LocalBinder mLocalBinder = (SensorService.LocalBinder) iBinder;
+            mLocalService = mLocalBinder.getService();
+            isBound = true;
+            Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBound = false;
+            Toast.makeText(MainActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,93 +66,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         editTextFilename = (EditText) findViewById(R.id.editTextFilename);
         editTextDescription = (EditText) findViewById(R.id.editTextDescription);
         buttonTracking = (Button) findViewById(R.id.buttonTracking);
-
-        // Get the root directory
-        // .. This is the Device Storage
-        File root = android.os.Environment.getExternalStorageDirectory();
-        dir = new File(root.getAbsolutePath() + "/DataToCSV");              // Get the path to the new directory to create
-        if (!dir.exists()) {                                                // Check if this directory exists
-            dir.mkdirs();                                                   // Create a new directory called DataToCSV
-        }
-
-        mSensorManger = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = mSensorManger.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManger.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     public void toggleTracking(View view) {
         if (isTracking) {
             isTracking = false;
+            doUnbindService();
             buttonTracking.setText("Start Tracking");
-            try {
-                writer.close();                                                 // Close writer stream
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         } else {
-            isTracking = true;
-            buttonTracking.setText("Stop Tracking");
-
-            String fileName = editTextFilename.getText().toString() + ".csv";   // Get the filename
+            String fileName = editTextFilename.getText().toString() + ".csv";
             String description = editTextDescription.getText().toString();
-
-            File f = new File(dir, fileName);                                   // Create a new file
-            String filePath = f.getAbsolutePath();                              // Get the absolute path to the file
-            writer = null;                                                      // Create the CSVWrite object which is from the opencsv library
-            if (f.exists() && !f.isDirectory()) {
-                try {
-                    mFileWriter = new FileWriter(filePath, true);              // Initialize file writer
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                writer = new CSVWriter(mFileWriter);                            // Initialize opencsv writer
-            } else {
-                try {
-                    writer = new CSVWriter(new FileWriter(filePath));           // Initialize opencsv writer
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            // Write Headers
-            dataContainer[3] = description;
-            writer.writeNext(dataContainer);
+            isTracking = true;
+            doBindService(fileName, description);
+            buttonTracking.setText("Stop Tracking");
         }
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        // Only write to csv if app isTracking
-        if (isTracking) {
-
-            // THIS IS WHERE THE DATA IS WRITTEN //
-            double ValueX = (Math.round(event.values[0]*1000)/1000.0);
-            double ValueY = (Math.round(event.values[1]*1000)/1000.0);
-            double ValueZ = (Math.round(event.values[2]*1000)/1000.0);
-
-            dataContainer[0] = "" + ValueX;
-            dataContainer[1] = "" + ValueY;
-            dataContainer[2] = "" + ValueZ;
-            dataContainer[3] = "...";
-
-            writer.writeNext(dataContainer);
-            ///////////////////////////////////////
-        }
+    void doBindService(String filename, String description) {
+        Intent intent = new Intent(this, SensorService.class);
+        intent.putExtra("filename", filename);
+        intent.putExtra("description", description);
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+    void doUnbindService()
+    {
+        unbindService(myConnection);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        try {
-            writer.close();                                                 // Close writer stream
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
-
---
+++ DataToCSV/app/src/main/java/shantanubobhate/datatocsv/SensorService.java
@@ -0,0 +1,143 @@
+package shantanubobhate.datatocsv;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SensorService extends Service implements SensorEventListener {
+
+    private FileWriter mFileWriter;
+    private File dir;
+    private CSVWriter writer;
+    private String[] dataContainer = {"Acceleration X", "Acceleration Y", "Acceleration Z", "..."};
+
+    // TAG to identify notification
+    private static final int NOTIFICATION = 007;
+
+    // IBinder object to allow Activity to connect
+    private final IBinder mBinder = new LocalBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        String fileName = intent.getStringExtra("filename");
+        String description = intent.getStringExtra("description");
+
+        // Get the root directory
+        // .. This is the Device Storage
+        File root = android.os.Environment.getExternalStorageDirectory();
+        dir = new File(root.getAbsolutePath() + "/DataToCSV");              // Get the path to the new directory to create
+        if (!dir.exists()) {                                                // Check if this directory exists
+            dir.mkdirs();                                                   // Create a new directory called DataToCSV
+        }
+
+        File f = new File(dir, fileName);                                   // Create a new file
+        String filePath = f.getAbsolutePath();                              // Get the absolute path to the file
+        writer = null;                                                      // Create the CSVWrite object which is from the opencsv library
+        if (f.exists() && !f.isDirectory()) {
+            try {
+                mFileWriter = new FileWriter(filePath, true);              // Initialize file writer
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            writer = new CSVWriter(mFileWriter);                            // Initialize opencsv writer
+        } else {
+            try {
+                writer = new CSVWriter(new FileWriter(filePath));           // Initialize opencsv writer
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        dataContainer[3] = description;
+        writer.writeNext(dataContainer);
+
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        try {
+            writer.close();                                                 // Close writer stream
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return super.onUnbind(intent);
+    }
+
+    public class LocalBinder extends Binder {
+        public SensorService getService() {
+            return SensorService.this;
+        }
+    }
+
+    private Sensor accelerometer;
+    private SensorManager mSensorManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        showNotification();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);                            // Unregister sensor when not in use
+
+        mNotificationManager.cancel(NOTIFICATION);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // Get acceleration values
+        dataContainer[0] = "" + (Math.round(sensorEvent.values[0]*1000)/1000.0);
+        dataContainer[1] = "" + (Math.round(sensorEvent.values[1]*1000)/1000.0);
+        dataContainer[1] = "" + (Math.round(sensorEvent.values[2]*1000)/1000.0);
+        writer.writeNext(dataContainer);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    private NotificationManager mNotificationManager;
+
+    private void showNotification() {
+        CharSequence text = "Sensor Service Started";
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            Notification mNotification = new Notification.Builder(this)
+                    .setSmallIcon(R.drawable.bell)
+                    .setTicker(text)
+                    .setContentTitle("Hello there!")
+                    .setContentText(text)
+                    .setContentIntent(contentIntent)
+                    .build();
+
+            mNotificationManager.notify(NOTIFICATION, mNotification);
+        }
+    }
+}
